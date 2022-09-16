c20637232bd12be97ec8dfec04314bf2325bd050
Vvargaxx <72069241+Vvargaxx@users.noreply.github.com>
2022-09-16 09:58:23 +0100

    Update main.c

--- main.c
+++ main.c
@@ -9,14 +9,19 @@ HEX Calculator, still in progress
 int main()
 {
     char hex[16] = {'0','1','2','3','4','5','6','7','8','9','A','B','C','D','E','F'};
-    
     char result[8];
     
-    int num = 460;
+    printf("********************************************* \n\
+**Bienvenido a la calculadora Hexadecimal****\n\
+********************************************* \n\
+Introduce el número decimal que quieres convertir: ");
+    
+    int num;
+    scanf("%d", &num);
     bool check = true;
     int i = 7;
     
-    while (num > 16){
+    while (check){
        int rest = num % 16;
        result[i] =  hex[rest];
        i--;
@@ -27,9 +32,10 @@ int main()
        }
     }
     result[i] =  hex[num%16];
+    printf("El resultado es: "); 
     for (int j = 0; j < 8; j++){
        printf("%c", result[j]); 
     }
-    
+    printf("\n");
     return 0;
 }
